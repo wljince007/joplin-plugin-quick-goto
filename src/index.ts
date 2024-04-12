7cfbf4250f5e3748886c1cefe6be8c9e3ee7dfbb
wljince007 <liangwang007@gmail.com>
2024-04-12 22:36:12 +0800

    auto set title as alias

--- src/index.ts
+++ src/index.ts
@@ -116,9 +116,7 @@ joplin.plugins.register({
 		for (let i = 1; i <= slots; i++) {
 			let settId    = `qgoto_item${i}ID` ;
 			let settAlias = `qgoto_item${i}Alias` ;
-			let settAliasValue = `Item ${i}`
-			
-			console.debug(`settId:${settId}`);
+			let settAliasDefaultValue = `Item ${i}`
 
 			await joplin.settings.registerSettings({
 				[settId]: {
@@ -131,7 +129,7 @@ joplin.plugins.register({
 					description: ''
 				},
 				[settAlias]: {
-					value: settAliasValue,
+					value: settAliasDefaultValue,
 					type: SettingItemType.String,
 					section: 'settings.quickGoto',
 					public: true,
@@ -142,20 +140,21 @@ joplin.plugins.register({
 			});
 
 
-			try {
-				var val  = await joplin.settings.value(settId) as string;
+			// auto set title as alias
+			var val  = await joplin.settings.value(settId) as string || '';
+			var valAlias  = await joplin.settings.value(settAlias) as string;
+			// console.debug(`val:${val},valAlias:${valAlias}`);
+			if (val !== '' && (valAlias === settAliasDefaultValue) || valAlias === '') {
 				var hashIdx = val.indexOf("#");
 				var noteId  = hashIdx < 0 ? val : val.substring(0,hashIdx);
-				var hash    = hashIdx < 0 ? ""  : val.substring(hashIdx+1);
 				console.debug(`settId:${settId}, val:${val}, noteId:${noteId}`);
 				if (noteId.length > 0) {
 						const note = await joplin.data.get(['notes', noteId], { fields: ['id', 'title'] });
-						console.debug(`note:${note}`);
+						// console.debug(`note:${note}`);
 						await joplin.settings.setValue(settAlias, note.title);
 				}
-			} catch (err) {
-				console.error(`err:${err}`)
 			}
+			
 
 			let cmdJName  = 'gotoJItem' + i;
 			let cmdJLabel = 'Goto';
@@ -168,7 +167,7 @@ joplin.plugins.register({
 			const alias = await joplin.settings.value(settAlias) as string;
 			if (alias) {
 				cmdJLabel = `Goto ${alias}`
-				cmdALabel = `Assign-${alias}`
+				cmdALabel = `Goto ${alias} - Assign`
 			}
 
 			await joplin.commands.register({
